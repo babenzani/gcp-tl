814b675edb6cfec433289aa0583aff523509d315
babenzani <33708074+babenzani@users.noreply.github.com>
2021-02-22 14:51:41 +0700

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,7 @@
 terraform {
   backend "gcs" {
-    credentials = "gcp-test-all-vpc-1706a367bad4.json"
+    credentials = file(var.credentials)
+    #credentials = "gcp-test-all-vpc-1706a367bad4.json"
     bucket  = "test-jkt-123"
     prefix  = "compute_engine/state"
   }
