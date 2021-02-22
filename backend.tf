bf26af4bb09ac62fc9c5e82cea7040eebe3f7c49
babenzani <33708074+babenzani@users.noreply.github.com>
2021-02-22 15:03:29 +0700

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "gcs" {
-    credentials = file(var.credentials)
-    #credentials = "gcp-test-all-vpc-1706a367bad4.json"
-    bucket  = "test-jkt-123"
-    prefix  = "compute_engine/state"
-  }
-}
